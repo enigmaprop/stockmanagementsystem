d5578301f1da25dcf6e83e7e076fbb34dea16f49
tamer fakhoury <tamer.fakho@gmail.com>
2023-05-14 18:38:24 +0200

    starting with logic

---
+++ models/order.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const orderSchame = new mongoose.Schema({
+    id:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:String,
+        required:true
+    },
+    products:[
+        {
+            id:{
+                type:String,
+                required:true
+            },
+            name:{
+                type:String,
+                required:true
+            },
+            price:{
+                type:Number,
+                required:true
+            },
+            quantity:{
+                type:Number,
+                required:true
+            }
+        }
+    ]
+    
+})
+
+const Order = mongoose.model('orders' , orderSchame);
+
+module.exports = Order;
